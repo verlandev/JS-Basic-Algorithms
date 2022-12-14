774189f60835ab4d2b601b1e35ce8005eca50b8d
Bemart93 <bemart93@gmail.com>
2022-12-14 19:14:57 +0100

    Iteracion#3

---
+++ ejercicio-2-variablesAvanzadas.js
@@ -0,0 +1,10 @@
+const character = {
+    name:"Jack Sparrow", 
+    age:25
+};
+
+let firstName = 'Jon';
+let lastName = 'Snow';
+let age = 24;
+console.log('Soy ' + firstName + ' ' + lastName + ", tengo " + age + " años y me gustan los lobos.");
+
